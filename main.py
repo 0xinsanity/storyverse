501bb717efdcf4f2d15eb987ce814271f8feea92
0xinsanity <noahsahar@gmail.com>
2023-11-06 17:40:23 -0800

    remove uvicorn

--- main.py
+++ main.py
@@ -242,6 +242,3 @@ async def quiz_response(request_body: QuizResponseRequestBody):
         "next_question": ""
     }
 
-
-if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
